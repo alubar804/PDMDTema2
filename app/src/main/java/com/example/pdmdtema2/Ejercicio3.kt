fbcf8297225eb1587c8841011c87187167b34c85
alu10216804 <alubar804@ieselcaminas.org>
2021-10-28 19:52:29 +0200

    3 acabado

--- app/src/main/java/com/example/pdmdtema2/Ejercicio3.kt
+++ app/src/main/java/com/example/pdmdtema2/Ejercicio3.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_ejercicio3.*
+import java.lang.Math.sqrt
 
 class Ejercicio3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,9 +13,10 @@ class Ejercicio3 : AppCompatActivity() {
         setContentView(R.layout.activity_ejercicio3)//EJ3Resultado
 
         EJ3button.setOnClickListener {
-            val cateto1 = EJ3in1.text.toString().toInt()
-            val cateto2 = EJ3in2.text.toString().toInt()
-            val hipotenusa=((cateto1*cateto1)+(cateto2*cateto2))
+            val cateto1 = EJ3in1.text.toString().toDouble()
+            val cateto2 = EJ3in2.text.toString().toDouble()
+            val hipotenusa=sqrt((cateto1*cateto1)+(cateto2*cateto2))
+            EJ3Resultado.text="Hipotenusa \n $hipotenusa"
         }
     }
 }
