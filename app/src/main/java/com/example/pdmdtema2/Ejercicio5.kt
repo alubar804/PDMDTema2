46ec18b8bd85a46b5ddf83cf0ec6cc515259532c
alubar804 <alubar804@ieselcaminas.org>
2021-11-03 18:39:07 +0100

    Ej 5 completo

--- app/src/main/java/com/example/pdmdtema2/Ejercicio5.kt
+++ app/src/main/java/com/example/pdmdtema2/Ejercicio5.kt
@@ -2,10 +2,73 @@ package com.example.pdmdtema2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.widget.TableLayout
+import kotlinx.android.synthetic.main.activity_ejercicio5.*
+
 
 class Ejercicio5 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ejercicio5)
+        var numSectreto = 0
+        val mlayout = findViewById<TableLayout>(R.id.laidout);
+        EJ5buttonReload.setOnClickListener {
+            numSectreto = (1..6).random()
+            //esto lo muestro para que sea mas facil identificar si funciona bien
+            EJ5Resultado.text=numSectreto.toString()
+        }
+        EJ5button1.setOnClickListener {
+            if (numSectreto == 1) {
+                mlayout.setBackgroundColor(getResources().getColor(R.color.red));
+                EJ5Resultado.text="BANG"
+            }else{
+                mlayout.setBackgroundColor(getResources().getColor(R.color.blue));
+            }
+        }
+        EJ5button2.setOnClickListener {
+            if (numSectreto == 2) {
+                mlayout.setBackgroundColor(getResources().getColor(R.color.red));
+                EJ5Resultado.text="BANG"
+            }else{
+                mlayout.setBackgroundColor(getResources().getColor(R.color.blue));
+            }
+        }
+        EJ5button3.setOnClickListener {
+            if (numSectreto == 3) {
+                mlayout.setBackgroundColor(getResources().getColor(R.color.red));
+                EJ5Resultado.text="BANG"
+            }else{
+                mlayout.setBackgroundColor(getResources().getColor(R.color.blue));
+            }
+
+        }
+        EJ5button4.setOnClickListener {
+            if (numSectreto == 4) {
+                mlayout.setBackgroundColor(getResources().getColor(R.color.red));
+                EJ5Resultado.text="BANG"
+            }else{
+                mlayout.setBackgroundColor(getResources().getColor(R.color.blue));
+            }
+
+        }
+        EJ5button5.setOnClickListener {
+            if (numSectreto == 5) {
+                mlayout.setBackgroundColor(getResources().getColor(R.color.red));
+                EJ5Resultado.text="BANG"
+            }else{
+                mlayout.setBackgroundColor(getResources().getColor(R.color.blue));
+            }
+
+        }
+        EJ5button6.setOnClickListener {
+            if (numSectreto == 6) {
+                mlayout.setBackgroundColor(getResources().getColor(R.color.red));
+                EJ5Resultado.text="BANG"
+            }else{
+                mlayout.setBackgroundColor(getResources().getColor(R.color.blue));
+            }
+
+        }
     }
 }
